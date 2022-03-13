01f812045ec0240372a08d1c3ff9a7a0243d8bf1
Josepablo C. B <josepablo134@gmail.com>
2022-03-13 14:54:21 -0600

    feat(X11): Xorg nvidia settings (GL503VD)

---
+++ sources/XorgSettings/disable_nvidia.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+ORIGIN=/etc/X11/xorg.conf_intel
+
+sudo cp $ORIGIN /etc/X11/xorg.conf
---
+++ sources/XorgSettings/enable_nvidia.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+ORIGIN=/etc/X11/xorg.conf_nvidia
+
+sudo cp $ORIGIN /etc/X11/xorg.conf
